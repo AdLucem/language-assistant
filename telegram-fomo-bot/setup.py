26b77396d4749f4cdae225add1f0ee2016a0dbbf
AdLucem <ghosalatreyee@gmail.com>
2021-05-30 20:35:51 +0530

    backup commit

--- telegram-fomo-bot/setup.py
+++ telegram-fomo-bot/setup.py
@@ -1,16 +1,48 @@
 """This script creates a directory, `.config`, which stores all the config variables required for the app. Config folder is not uploaded to the github; this is to prevent the config variables being made public."""
 
-configInputList = [
-    "Bot API token",
-]
+from os.path import isfile, isdir, join
+from os import mkdir
+import json
 
-if __name__ == "__main__":
 
-    configInputs = {}
+def readConfigs(configInputList):
+
+    configs = {}
 
     for question in configInputList:
 
         answer = input(question + " : ")
-        configInputs[question] = answer
+        configs[question] = answer
 
-    print(configInputs) 
+    return configs
+
+
+def writeConfigs(configs, dirname):
+
+    dirExists = isdir(dirname)
+
+    if not dirExists:
+        mkdir(dirname)
+
+    with open(join(dirname, "config.json"), "w+") as f:
+        json.dump(configs, f)
+        return 0
+
+    return 1
+
+
+def main():
+
+    configInputList = [
+        "Bot API token",
+    ]
+
+    configs = readConfigs(configInputList)
+    success = writeConfigs(configs, "./.config")
+    print(success)
+
+
+# --------- run the whole damn thing --------------------
+main()
+
+    
